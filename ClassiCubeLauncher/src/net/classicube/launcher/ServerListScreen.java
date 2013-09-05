528b2d57d5e60eea11ce4b5e80aae666b01cfdd3
fragmer <fragmer@gmail.com>
2013-09-04 18:19:44 -0600

    More tweaks to ServerListScreen

--- ClassiCubeLauncher/src/net/classicube/launcher/ServerListScreen.java
+++ ClassiCubeLauncher/src/net/classicube/launcher/ServerListScreen.java
@@ -18,15 +18,15 @@ public class ServerListScreen extends javax.swing.JFrame {
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jSeparator1 = new javax.swing.JSeparator();
-        jSeparator2 = new javax.swing.JSeparator();
-        jButton2 = new javax.swing.JButton();
-        jTextField2 = new javax.swing.JTextField();
-        jButton3 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        bChangeUser = new javax.swing.JButton();
+        tSearch = new javax.swing.JTextField();
+        separator1 = new javax.swing.JSeparator();
+        separator2 = new javax.swing.JSeparator();
+        bPreferences = new javax.swing.JButton();
+        tServerURL = new javax.swing.JTextField();
+        bConnect = new javax.swing.JButton();
+        serverTableContainer = new javax.swing.JScrollPane();
+        serverTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         java.awt.GridBagLayout layout = new java.awt.GridBagLayout();
@@ -34,64 +34,64 @@ public class ServerListScreen extends javax.swing.JFrame {
         layout.rowHeights = new int[] {0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0};
         getContentPane().setLayout(layout);
 
-        jButton1.setText("< Change User");
+        bChangeUser.setText("< Change User");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
-        getContentPane().add(jButton1, gridBagConstraints);
+        getContentPane().add(bChangeUser, gridBagConstraints);
 
-        jTextField1.setText("Search");
+        tSearch.setText("Search");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 4;
         gridBagConstraints.gridwidth = 5;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.PAGE_END;
-        getContentPane().add(jTextField1, gridBagConstraints);
+        getContentPane().add(tSearch, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.gridwidth = 5;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        getContentPane().add(jSeparator1, gridBagConstraints);
+        getContentPane().add(separator1, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 8;
         gridBagConstraints.gridwidth = 5;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        getContentPane().add(jSeparator2, gridBagConstraints);
+        getContentPane().add(separator2, gridBagConstraints);
 
-        jButton2.setText("Preferences");
+        bPreferences.setText("Preferences");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 4;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_END;
-        getContentPane().add(jButton2, gridBagConstraints);
+        getContentPane().add(bPreferences, gridBagConstraints);
 
-        jTextField2.setText("Server URL");
+        tServerURL.setText("Server URL");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 10;
         gridBagConstraints.gridwidth = 3;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 0.1;
-        getContentPane().add(jTextField2, gridBagConstraints);
+        getContentPane().add(tServerURL, gridBagConstraints);
 
-        jButton3.setText("Connect >");
+        bConnect.setText("Connect >");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 4;
         gridBagConstraints.gridy = 10;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LAST_LINE_END;
-        getContentPane().add(jButton3, gridBagConstraints);
+        getContentPane().add(bConnect, gridBagConstraints);
 
-        jScrollPane1.setMinimumSize(new java.awt.Dimension(302, 152));
-        jScrollPane1.setPreferredSize(new java.awt.Dimension(402, 402));
+        serverTableContainer.setMinimumSize(new java.awt.Dimension(302, 152));
+        serverTableContainer.setPreferredSize(new java.awt.Dimension(402, 402));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        serverTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -114,72 +114,38 @@ public class ServerListScreen extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jTable1.setColumnSelectionAllowed(true);
-        jTable1.getTableHeader().setReorderingAllowed(false);
-        jScrollPane1.setViewportView(jTable1);
-        jTable1.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        jTable1.getColumnModel().getColumn(1).setResizable(false);
-        jTable1.getColumnModel().getColumn(1).setPreferredWidth(60);
-        jTable1.getColumnModel().getColumn(2).setResizable(false);
-        jTable1.getColumnModel().getColumn(2).setPreferredWidth(60);
-        jTable1.getColumnModel().getColumn(3).setResizable(false);
-        jTable1.getColumnModel().getColumn(3).setPreferredWidth(60);
-        jTable1.getColumnModel().getColumn(4).setResizable(false);
-        jTable1.getColumnModel().getColumn(4).setPreferredWidth(60);
+        serverTable.setColumnSelectionAllowed(true);
+        serverTable.getTableHeader().setReorderingAllowed(false);
+        serverTableContainer.setViewportView(serverTable);
+        serverTable.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        serverTable.getColumnModel().getColumn(1).setResizable(false);
+        serverTable.getColumnModel().getColumn(1).setPreferredWidth(60);
+        serverTable.getColumnModel().getColumn(2).setResizable(false);
+        serverTable.getColumnModel().getColumn(2).setPreferredWidth(60);
+        serverTable.getColumnModel().getColumn(3).setResizable(false);
+        serverTable.getColumnModel().getColumn(3).setPreferredWidth(60);
+        serverTable.getColumnModel().getColumn(4).setResizable(false);
+        serverTable.getColumnModel().getColumn(4).setPreferredWidth(60);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 6;
         gridBagConstraints.gridwidth = 5;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        getContentPane().add(jScrollPane1, gridBagConstraints);
+        getContentPane().add(serverTableContainer, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ServerListScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ServerListScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ServerListScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ServerListScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ServerListScreen().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JSeparator jSeparator2;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton bChangeUser;
+    private javax.swing.JButton bConnect;
+    private javax.swing.JButton bPreferences;
+    private javax.swing.JSeparator separator1;
+    private javax.swing.JSeparator separator2;
+    private javax.swing.JTable serverTable;
+    private javax.swing.JScrollPane serverTableContainer;
+    private javax.swing.JTextField tSearch;
+    private javax.swing.JTextField tServerURL;
     // End of variables declaration//GEN-END:variables
 }
