77bac0cc559f4c370878f6893e3b521fe6fc9ada
fragmer <fragmer@gmail.com>
2013-09-14 19:14:09 -0600

    hashgen now takes care of packing jars (pack200+lzma)

--- HashGen/hashgen.py
+++ HashGen/hashgen.py
@@ -1,18 +1,25 @@
 #! /usr/bin/env python
 import os
 import hashlib
+import tempfile
+from subprocess import call
 
 files = [
     'launcher.jar',
     'client.jar',
-    'lwjgl.jar.pack.lzma',
-    'lwjgl_util.jar.pack.lzma',
-    'jinput.jar.pack.lzma',
-    'windows_natives.jar.lzma',
-    'macosx_natives.jar.lzma',
-    'linux_natives.jar.lzma',
-    'solaris_natives.jar.lzma'
+    'lwjgl.jar',
+    'lwjgl_util.jar',
+    'jinput.jar',
+    'windows_natives.jar',
+    'macosx_natives.jar',
+    'linux_natives.jar',
+    'solaris_natives.jar'
 ]
+
+def packfile(file):
+    call("pack200 -E9 -g "+file+".pack "+file, shell=True)
+    call("lzma -9 "+file+".pack", shell=True)
+    return file+".pack.lzma"
 
 def hashfile(afile, hasher, blocksize=65536):
     buf = afile.read(blocksize)
@@ -23,4 +30,6 @@ def hashfile(afile, hasher, blocksize=65536):
 
 for file in files:
     hash = hashfile(open(file,'rb'),hashlib.sha1())
-    print file,os.path.getsize(file),hash
+    packedname = packfile(file)
+    print packedname,os.path.getsize(packedname),hash
+
