mstefarov/ClassiCubeLauncher

commit 0ee095b7b0766073d384ae558e33b73af8c47754

Updater can now process several files in parallel, significantly speeding up the first-run download, and any future big updates.

ClassiCubeLauncher/src/net/classicube/launcher/UpdateTask.java
@@ -32,8 +32,8 @@ public final class UpdateTask
     // =============================================================================================
     //                                                                    CONSTANTS & INITIALIZATION
     // =============================================================================================
+    private static final int MAX_PARALLEL_DOWNLOAD = 4;
     private static final UpdateTask instance = new UpdateTask();
-    private boolean updatesApplied;
 
     public static UpdateTask getInstance() {
         return instance;
@@ -45,6 +45,12 @@ public final class UpdateTask
     // =============================================================================================
     //                                                                                          MAIN
     // =============================================================================================
+    private Thread[] workerThreads;
+    private List<FileToDownload> files;
+    private int activeFileNumber, filesDone, totalFiles;
+    private boolean needLzma;
+    private boolean updatesApplied;
+
     @Override
     protected Boolean doInBackground()
             throws Exception {
@@ -53,7 +59,7 @@ public final class UpdateTask
 
         // step 1: build up file list
         logger.log(Level.INFO, "Checking for updates.");
-        final List<FileToDownload> files = pickBinariesToDownload();
+        files = pickBinariesToDownload();
         files.addAll(pickResourcesToDownload());
 
         if (files.isEmpty()) {
@@ -65,33 +71,43 @@ public final class UpdateTask
 
             this.activeFileNumber = 0;
             this.totalFiles = files.size();
-            for (final FileToDownload file : files) {
-                this.activeFile = file;
-                try {
-                    // step 2: download
-                    signalDownloadProgress();
-                    final File downloadedFile = downloadFile(file);
 
-                    // step 3: unpack
-                    signalUnpackProgress();
-                    final File processedFile = SharedUpdaterCode.processDownload(
-                            LogUtil.getLogger(),
-                            downloadedFile, file.remoteUrl, file.localName.getName());
-
-                    // step 4: deploy
-                    deployFile(processedFile, file.localName);
-
-                } catch (final IOException ex) {
-                    logger.log(Level.SEVERE, "Error downloading an updated file.", ex);
-                }
-                this.activeFileNumber++;
+            if (needLzma) {
+                // lzma.jar will always be the first on the list.
+                // We need to get it before deploying any other files.
+                processOneFile(getNextFileSync());
             }
-            logger.log(Level.INFO, "Updates applied.");
+
+            int numThreads = Math.min(totalFiles, MAX_PARALLEL_DOWNLOAD);
+            workerThreads = new Thread[numThreads];
+            for (int i = 0; i < numThreads; i++) {
+                workerThreads[i] = new DownloadThread(logger);
+                workerThreads[i].start();
+            }
+            for (int i = 0; i < numThreads; i++) {
+                workerThreads[i].join();
+            }
         }
 
         // confirm that all required files have been downloaded and deployed
         verifyFiles(files);
+
+        logger.log(Level.INFO, "Updates applied.");
         return true;
+    }
+
+    private void processOneFile(final FileToDownload file)
+            throws InterruptedException, IOException {
+        // step 1: download
+        final File downloadedFile = downloadFile(file);
+
+        // step 2: unpack
+        final File processedFile = SharedUpdaterCode.processDownload(
+                LogUtil.getLogger(),
+                downloadedFile, file.remoteUrl, file.localName.getName());
+
+        // step 3: deploy
+        deployFile(processedFile, file.localName);
     }
 
     private static String listFileNames(final List<FileToDownload> files) {
@@ -106,6 +122,29 @@ public final class UpdateTask
         }
         return sb.toString();
     }
+
+    // Called by DownloadThreads to get the next file. Pushes an update 
+    // Returns null when there are no more files left to download.
+    private synchronized FileToDownload getNextFileSync() {
+        filesDone++;
+        FileToDownload fileToReturn = null;
+        String fileNameToReport;
+
+        if (activeFileNumber != totalFiles) {
+            fileToReturn = files.get(activeFileNumber);
+            activeFileNumber++;
+            fileNameToReport = fileToReturn.localName.getName();
+        } else {
+            fileNameToReport = files.get(totalFiles - 1).localName.getName();
+        }
+
+        int overallProgress = (this.filesDone * 100 + 100) / this.totalFiles;
+        final String status = String.format("Updating %s (%d/%d)",
+                fileNameToReport, this.activeFileNumber, this.totalFiles);
+        this.publish(new ProgressUpdate(status, overallProgress));
+        return fileToReturn;
+    }
+
     // =============================================================================================
     //                                                                        CHECKING / DOWNLOADING
     // =============================================================================================
@@ -139,16 +178,16 @@ public final class UpdateTask
 
     private List<FileToDownload> pickResourcesToDownload()
             throws IOException {
-        final List<FileToDownload> files = new ArrayList<>();
+        final List<FileToDownload> pickedFiles = new ArrayList<>();
 
         final File resDir = new File(PathUtil.getClientDir(), "resources");
         for (final String resFileName : resourceFiles) {
             final File resFile = new File(resDir, resFileName);
             if (!resFile.exists()) {
-                files.add(new FileToDownload(RESOURCE_DOWNLOAD_URL, resFileName, resFile));
+                pickedFiles.add(new FileToDownload(RESOURCE_DOWNLOAD_URL, resFileName, resFile));
             }
         }
-        return files;
+        return pickedFiles;
     }
 
     private List<FileToDownload> pickBinariesToDownload()
@@ -176,7 +215,7 @@ public final class UpdateTask
                             localFile.localName.getName());
                     download = true;
                 } else {
-                    throw new RuntimeException("Required file \"" + localFile.remoteUrl + "\" does not exist.");
+                    throw new RuntimeException("Required file \"" + localFile.remoteUrl + "\" cannot be found.");
                 }
             } else if (updateExistingFiles && !isLzma) {
                 if (remoteFile != null) {
@@ -200,7 +239,11 @@ public final class UpdateTask
             }
             if (download) {
                 localFile.remoteLength = remoteFile.length;
-                filesToDownload.add(localFile);
+                if (isLzma) {
+                    needLzma = true;
+                } else {
+                    filesToDownload.add(localFile);
+                }
             }
         }
         return filesToDownload;
@@ -208,35 +251,35 @@ public final class UpdateTask
 
     private List<FileToDownload> listBinaries()
             throws IOException {
-        final List<FileToDownload> files = new ArrayList<>();
+        final List<FileToDownload> binaryFiles = new ArrayList<>();
 
         final File clientDir = PathUtil.getClientDir();
         final File launcherDir = SharedUpdaterCode.getLauncherDir();
 
-        files.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
+        binaryFiles.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
                 "lzma.jar",
                 new File(launcherDir, "lzma.jar")));
 
-        files.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
+        binaryFiles.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
                 "launcher.jar.pack.lzma",
                 new File(launcherDir, SharedUpdaterCode.LAUNCHER_NEW_JAR_NAME)));
 
-        files.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
+        binaryFiles.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
                 "client.jar.pack.lzma",
                 new File(clientDir, "client.jar")));
 
-        files.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
+        binaryFiles.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
                 "lwjgl.jar.pack.lzma",
                 new File(clientDir, "libs/lwjgl.jar")));
-        files.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
+        binaryFiles.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
                 "lwjgl_util.jar.pack.lzma",
                 new File(clientDir, "libs/lwjgl_util.jar")));
-        files.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
+        binaryFiles.add(new FileToDownload(SharedUpdaterCode.BASE_URL,
                 "jinput.jar.pack.lzma",
                 new File(clientDir, "libs/jinput.jar")));
-        files.add(pickNativeDownload());
+        binaryFiles.add(pickNativeDownload());
 
-        return files;
+        return binaryFiles;
     }
 
     // get a list of files available from CC.net
@@ -339,16 +382,12 @@ public final class UpdateTask
             final URLConnection connection = website.openConnection();
             file.remoteLength = connection.getContentLength();
         }
-        signalDownloadPercent(0, file.remoteLength);
 
         try (final InputStream siteIn = website.openStream()) {
             try (final FileOutputStream fileOut = new FileOutputStream(tempFile)) {
                 int len;
-                int total = 0;
                 while ((len = siteIn.read(ioBuffer)) > 0) {
                     fileOut.write(ioBuffer, 0, len);
-                    total += len;
-                    signalDownloadPercent(total, file.remoteLength);
                 }
             }
         }
@@ -424,8 +463,6 @@ public final class UpdateTask
     //                                                                            PROGRESS REPORTING
     // =============================================================================================
     private volatile UpdateScreen updateScreen;
-    private int activeFileNumber, totalFiles;
-    private FileToDownload activeFile;
 
     @Override
     protected synchronized void process(final List<ProgressUpdate> chunks) {
@@ -441,45 +478,12 @@ public final class UpdateTask
         if (fileName == null) {
             throw new NullPointerException("fileName");
         }
-        final int overallProgress = -1; // between 0 and 15%
-        final String status = "Checking for updates...";
-        this.publish(new ProgressUpdate(fileName, status, overallProgress));
-    }
-
-    private void signalDownloadProgress() {
-        final int overallProgress = (this.activeFileNumber * 100) / totalFiles;
-        final String fileName = activeFile.localName.getName() + " (" + (this.activeFileNumber + 1) + " / " + this.totalFiles + ")";
-        final String status = "Preparing to download...";
-        this.publish(new ProgressUpdate(fileName, status, overallProgress));
-    }
-
-    private void signalDownloadPercent(final long bytesSoFar, final long bytesTotal) {
-        final String fileName = activeFile.localName.getName() + " (" + (this.activeFileNumber + 1) + " / " + this.totalFiles + ")";
-        final String status;
-        final int overallProgress;
-        if (bytesTotal > 0) {
-            final int percent = (int) Math.max(0, Math.min(100, (bytesSoFar * 100) / bytesTotal));
-            final int baseProgress = (this.activeFileNumber * 100) / this.totalFiles;
-            final int deltaProgress = 100 / this.totalFiles;
-            overallProgress = baseProgress + (deltaProgress * percent) / 100;
-            status = String.format("Downloading (%s / %s KiB)", bytesSoFar / 1024, bytesTotal / 1024);
-        } else {
-            status = String.format("Downloading... (%s KiB)", bytesSoFar / 1024);
-            overallProgress = (this.activeFileNumber * 100) / this.totalFiles;
-        }
-        this.publish(new ProgressUpdate(fileName, status, overallProgress));
-    }
-
-    private void signalUnpackProgress() {
-        int overallProgress = (this.activeFileNumber * 100 + 100) / this.totalFiles;
-        final String fileName = activeFile.localName.getName() + " (" + (this.activeFileNumber + 1) + " / " + this.totalFiles + ")";
-        final String status = "Unpacking...";
-        this.publish(new ProgressUpdate(fileName, status, overallProgress));
+        this.publish(new ProgressUpdate("Checking " + fileName, -1));
     }
 
     private void signalDone() {
         final String message = (this.updatesApplied ? "Updates applied." : "No updates needed.");
-        this.publish(new ProgressUpdate(" ", message, 100));
+        this.publish(new ProgressUpdate(message, 100));
     }
 
     @Override
@@ -538,20 +542,40 @@ public final class UpdateTask
 
     public final static class ProgressUpdate {
 
-        public String fileName;
         public String statusString;
         public int progress;
 
-        public ProgressUpdate(final String fileName, final String statusString, final int progress) {
-            if (fileName == null) {
-                throw new NullPointerException("fileName");
-            }
+        public ProgressUpdate(final String statusString, final int progress) {
             if (statusString == null) {
                 throw new NullPointerException("statusString");
             }
-            this.fileName = fileName;
             this.statusString = statusString;
             this.progress = progress;
+        }
+    }
+
+    private class DownloadThread extends Thread {
+
+        private final Logger logger;
+
+        public DownloadThread(Logger logger) {
+            this.logger = logger;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    FileToDownload file = getNextFileSync();
+                    if (file == null) {
+                        return;
+                    }
+                    processOneFile(file);
+                }
+
+            } catch (final IOException | InterruptedException ex) {
+                logger.log(Level.SEVERE, "Error downloading an updated file.", ex);
+            }
         }
     }
 }

ClassiCubeLauncher/src/net/classicube/launcher/ClientLauncher.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import net.classicube.launcher.gui.DebugWindow;
 import net.classicube.launcher.gui.ErrorScreen;
+import org.apache.commons.lang3.StringUtils;
 
 // Handles launching the client process.
 final public class ClientLauncher {
@@ -70,7 +71,7 @@ final public class ClientLauncher {
             processBuilder.directory(PathUtil.getClientDir());
 
             // log the command used to launch client
-            String cmdLineToLog = concatStringsWSep(processBuilder.command(), " ");
+            String cmdLineToLog = StringUtils.join(processBuilder.command(), " ");
             if (joinInfo != null) {
                 cmdLineToLog = cmdLineToLog.replace(joinInfo.pass, "########"); // sanitize mppass
             }
@@ -110,21 +111,5 @@ final public class ClientLauncher {
             ErrorScreen.show("Could not launch the game",
                     "Error launching the client:<br>" + ex.getMessage(), ex);
         }
-    }
-
-    private static String concatStringsWSep(final List<String> strings, final String separator) {
-        if (strings == null) {
-            throw new NullPointerException("strings");
-        }
-        if (separator == null) {
-            throw new NullPointerException("separator");
-        }
-        final StringBuilder sb = new StringBuilder();
-        String sep = "";
-        for (final String s : strings) {
-            sb.append(sep).append(s);
-            sep = separator;
-        }
-        return sb.toString();
     }
 }

ClassiCubeLauncher/src/net/classicube/launcher/gui/UpdateScreen.form
@@ -24,19 +24,6 @@
 
   <Layout class="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout"/>
   <SubComponents>
-    <Component class="javax.swing.JLabel" name="lFileName">
-      <Properties>
-        <Property name="foreground" type="java.awt.Color" editor="org.netbeans.beaninfo.editors.ColorEditor">
-          <Color blue="ff" green="ff" red="ff" type="rgb"/>
-        </Property>
-        <Property name="text" type="java.lang.String" value="Preparing to update..."/>
-      </Properties>
-      <Constraints>
-        <Constraint layoutClass="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout" value="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout$GridBagConstraintsDescription">
-          <GridBagConstraints gridX="0" gridY="0" gridWidth="0" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="8" insetsRight="0" anchor="10" weightX="0.0" weightY="0.0"/>
-        </Constraint>
-      </Constraints>
-    </Component>
     <Component class="javax.swing.JLabel" name="lStats">
       <Properties>
         <Property name="foreground" type="java.awt.Color" editor="org.netbeans.beaninfo.editors.ColorEditor">
@@ -46,14 +33,14 @@
       </Properties>
       <Constraints>
         <Constraint layoutClass="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout" value="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout$GridBagConstraintsDescription">
-          <GridBagConstraints gridX="0" gridY="1" gridWidth="0" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="8" insetsRight="0" anchor="10" weightX="0.0" weightY="0.0"/>
+          <GridBagConstraints gridX="0" gridY="0" gridWidth="0" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="8" insetsRight="0" anchor="10" weightX="0.0" weightY="0.0"/>
         </Constraint>
       </Constraints>
     </Component>
     <Component class="javax.swing.JProgressBar" name="progress">
       <Constraints>
         <Constraint layoutClass="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout" value="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout$GridBagConstraintsDescription">
-          <GridBagConstraints gridX="0" gridY="2" gridWidth="0" gridHeight="1" fill="2" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="0" insetsRight="0" anchor="10" weightX="0.1" weightY="0.0"/>
+          <GridBagConstraints gridX="0" gridY="1" gridWidth="0" gridHeight="1" fill="2" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="0" insetsRight="0" anchor="10" weightX="0.1" weightY="0.0"/>
         </Constraint>
       </Constraints>
     </Component>
@@ -66,7 +53,7 @@
       </Properties>
       <Constraints>
         <Constraint layoutClass="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout" value="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout$GridBagConstraintsDescription">
-          <GridBagConstraints gridX="0" gridY="3" gridWidth="0" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="8" insetsLeft="0" insetsBottom="8" insetsRight="0" anchor="10" weightX="0.0" weightY="0.0"/>
+          <GridBagConstraints gridX="0" gridY="2" gridWidth="0" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="8" insetsLeft="0" insetsBottom="8" insetsRight="0" anchor="10" weightX="0.0" weightY="0.0"/>
         </Constraint>
       </Constraints>
     </Component>
@@ -80,7 +67,7 @@
       </Events>
       <Constraints>
         <Constraint layoutClass="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout" value="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout$GridBagConstraintsDescription">
-          <GridBagConstraints gridX="0" gridY="4" gridWidth="1" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="0" insetsRight="16" anchor="25" weightX="0.0" weightY="0.0"/>
+          <GridBagConstraints gridX="0" gridY="3" gridWidth="1" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="0" insetsRight="16" anchor="25" weightX="0.0" weightY="0.0"/>
         </Constraint>
       </Constraints>
     </Component>
@@ -94,7 +81,7 @@
       </Events>
       <Constraints>
         <Constraint layoutClass="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout" value="org.netbeans.modules.form.compat2.layouts.DesignGridBagLayout$GridBagConstraintsDescription">
-          <GridBagConstraints gridX="1" gridY="4" gridWidth="1" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="0" insetsRight="0" anchor="26" weightX="0.0" weightY="0.0"/>
+          <GridBagConstraints gridX="1" gridY="3" gridWidth="1" gridHeight="1" fill="0" ipadX="0" ipadY="0" insetsTop="0" insetsLeft="0" insetsBottom="0" insetsRight="0" anchor="26" weightX="0.0" weightY="0.0"/>
         </Constraint>
       </Constraints>
     </Component>

ClassiCubeLauncher/src/net/classicube/launcher/gui/UpdateScreen.java
@@ -78,7 +78,6 @@ public final class UpdateScreen extends JFrame {
             this.progress.setIndeterminate(false);
             this.progress.setValue(dl.progress);
         }
-        this.lFileName.setText(dl.fileName);
         this.lStats.setText(dl.statusString);
     }
 
@@ -143,7 +142,6 @@ public final class UpdateScreen extends JFrame {
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
-        lFileName = new javax.swing.JLabel();
         lStats = new javax.swing.JLabel();
         progress = new javax.swing.JProgressBar();
         lNotice = new javax.swing.JLabel();
@@ -154,26 +152,17 @@ public final class UpdateScreen extends JFrame {
         setTitle("Game Update");
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        lFileName.setForeground(new java.awt.Color(255, 255, 255));
-        lFileName.setText("Preparing to update...");
+        lStats.setForeground(new java.awt.Color(255, 255, 255));
+        lStats.setText("...");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 8, 0);
-        getContentPane().add(lFileName, gridBagConstraints);
-
-        lStats.setForeground(new java.awt.Color(255, 255, 255));
-        lStats.setText("...");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
-        gridBagConstraints.insets = new java.awt.Insets(0, 0, 8, 0);
         getContentPane().add(lStats, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridy = 1;
         gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 0.1;
@@ -183,7 +172,7 @@ public final class UpdateScreen extends JFrame {
         lNotice.setText("<notice>");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridy = 2;
         gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
         gridBagConstraints.insets = new java.awt.Insets(8, 0, 8, 0);
         getContentPane().add(lNotice, gridBagConstraints);
@@ -197,7 +186,7 @@ public final class UpdateScreen extends JFrame {
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LAST_LINE_START;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 16);
         getContentPane().add(bViewChanges, gridBagConstraints);
@@ -211,7 +200,7 @@ public final class UpdateScreen extends JFrame {
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LAST_LINE_END;
         getContentPane().add(bPlay, gridBagConstraints);
 
@@ -220,7 +209,6 @@ public final class UpdateScreen extends JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private net.classicube.launcher.gui.JNiceLookingButton bPlay;
     private net.classicube.launcher.gui.JNiceLookingButton bViewChanges;
-    private javax.swing.JLabel lFileName;
     private javax.swing.JLabel lNotice;
     private javax.swing.JLabel lStats;
     private javax.swing.JProgressBar progress;

commit 1efb32d8df0a6296b1876baad6d82b097197eaa0

Fixed a regression in 0ee095b7b0766073d384ae558e33b73af8c47754 that caused occasional download corruption.

ClassiCubeLauncher/src/net/classicube/launcher/UpdateTask.java
@@ -10,6 +10,8 @@ import java.io.InputStream;
 import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.util.ArrayList;
@@ -158,7 +160,6 @@ public final class UpdateTask
     //                                                                        CHECKING / DOWNLOADING
     // =============================================================================================
     private MessageDigest digest;
-    private final byte[] ioBuffer = new byte[64 * 1024];
     private final static String[] resourceFiles = new String[]{"music/calm1.ogg", "music/calm2.ogg", "music/calm3.ogg",
         "newmusic/hal1.ogg", "newmusic/hal2.ogg", "newmusic/hal3.ogg", "newmusic/hal4.ogg",
         "newsound/step/grass1.ogg", "newsound/step/grass2.ogg", "newsound/step/grass3.ogg",
@@ -365,6 +366,7 @@ public final class UpdateTask
             if (manifest == null) {
                 return "<none>";
             }
+            final byte[] ioBuffer = new byte[64 * 1024];
             try (final InputStream is = zipFile.getInputStream(manifest)) {
                 try (final DigestInputStream dis = new DigestInputStream(is, digest)) {
                     while (dis.read(ioBuffer) != -1) {
@@ -422,12 +424,9 @@ public final class UpdateTask
         final File tempFile = File.createTempFile(file.localName.getName(), ".downloaded");
         final URL website = new URL(file.baseUrl + file.remoteName);
 
-        try (final InputStream siteIn = website.openStream()) {
-            try (final FileOutputStream fileOut = new FileOutputStream(tempFile)) {
-                int len;
-                while ((len = siteIn.read(ioBuffer)) > 0) {
-                    fileOut.write(ioBuffer, 0, len);
-                }
+        try (ReadableByteChannel rbc = Channels.newChannel(website.openStream())) {
+            try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
             }
         }
         return tempFile;
@@ -673,7 +672,7 @@ public final class UpdateTask
 
             } catch (final Exception ex) {
                 String fileName = (file != null ? file.remoteName : "?");
-                logger.log(Level.SEVERE, "Error downloading an updated file: " + fileName, ex);
+                logger.log(Level.SEVERE, "Error downloading or deploying an updated file: " + fileName, ex);
             }
         }
     }